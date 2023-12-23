9a32dc499e7cc906d958efd7bade8010c101a9de
BelozerovYaroslav <yaroslav.belozerov.96@bk.ru>
2023-12-23 09:31:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double S = 0;
+            for (double i = 1; i <= 20; i++)
+            {
+                double p = 0;
+                for (double j = 1; j <= 10; j++)
+                {
+                    p += Sin(Pow(i, 3) + Pow(j, 4));
+                }
+                S += p;
+            }
+            Console.WriteLine(S);
+        }
+    }
+}
